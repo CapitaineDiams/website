6bb9b0b523af94a858cd52052cb38418016ccaf6
Simon Le Goff <123492669+CapitaineDiams@users.noreply.github.com>
2025-05-19 19:11:29 +0200

    Update testcommande.js

--- Site_web/Scripts/testcommande.js
+++ Site_web/Scripts/testcommande.js
@@ -59,7 +59,7 @@ BTP.addEventListener("click", TheoremePy);
 let xhrbutton = document.getElementById("xhr");
 let fxhr = new XMLHttpRequest();
 
-function fxhr() {
+function xhr() {
   fxhr.open("POST", "http://91.197.6.229:32382/api/2/call");
   const body = JSON.stringify({
   name: "fs.read",
